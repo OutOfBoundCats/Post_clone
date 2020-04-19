92e4ff701a867365b95070287a2b84bff650bd0c
Raj Patil <floatraj@gmail.com>
2020-04-19 17:28:44 +0530

    Added Alerts

--- src/main/java/index/Login.java
+++ src/main/java/index/Login.java
@@ -4,11 +4,13 @@ package index;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.*;
+import javafx.stage.Window;
 public class Login {
 
 	static String[] TNS ;
@@ -47,7 +49,7 @@ public class Login {
 	}
 	
 	public void OnLoginClick() {
-		
+		Window owner = login_button.getScene().getWindow();
 		username=UserName.getText();
 		password=Password.getText();
 		TNS_String=TNS_ComboBox.getValue();
@@ -55,11 +57,19 @@ public class Login {
 		Main.setUsername(username);
 	    Main.setTNS_String(TNS_String);
 		System.out.println(username+" "+password+" "+TNS_String);
+		if(UserName.getText().isEmpty()) {
+			AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Empty Username", "Please Enter Username");
+		}else if(Password.getText().isEmpty()) {
+			AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Empty password", "Please Enter Password");
+		}if(TNS_ComboBox.getValue()== null) {
+			AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Empty TNS details", "Please Enter TNS Details");
+		}else {
+			UserName.setDisable(true);
+			Password.setDisable(true);
+			login_button.setDisable(true);
+			TNS_ComboBox.setDisable(true);
+		}
 		
-		UserName.setDisable(true);
-		Password.setDisable(true);
-		login_button.setDisable(true);
-		TNS_ComboBox.setDisable(true);
 		
 		
 		
